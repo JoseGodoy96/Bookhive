b5bc17252436914b09940204c9c01914383fbdbc
José María <josemariagodoy1996@gmail.com>
2026-09-18 16:24:34 +0200

    feat(): modificaciones principales

--- backend/bookhive/src/main/java/com/chema/bookhive/BookhiveApplication.java
+++ backend/bookhive/src/main/java/com/chema/bookhive/BookhiveApplication.java
@@ -1,4 +1,4 @@
-package com.example.bookhive;
+package com.chema.bookhive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/bookhive/src/test/java/com/chema/bookhive/BookhiveApplicationTests.java
+++ backend/bookhive/src/test/java/com/chema/bookhive/BookhiveApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.bookhive;
+package com.chema.bookhive;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
