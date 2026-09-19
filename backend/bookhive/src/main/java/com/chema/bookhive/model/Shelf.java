d1e072173e2528e7926af92f0a3c2d634a76d98b
José María <josemariagodoy1996@gmail.com>
2026-09-19 22:07:03 +0200

    feat(Shelf.java): la clase Shelf esta listo

--- backend/bookhive/src/main/java/com/chema/bookhive/model/Shelf.java
+++ backend/bookhive/src/main/java/com/chema/bookhive/model/Shelf.java
@@ -1,4 +1,29 @@
 package com.chema.bookhive.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "shelves")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Shelf {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
