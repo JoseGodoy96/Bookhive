122e8a9ee2a95eeab710db618c648d890f48ebf3
José María <josemariagodoy1996@gmail.com>
2026-09-20 21:39:17 +0200

    feat(UserRepository.java, BookRepository.java, ShelfRepository.java): Modificaciones para tener queries en repository

--- backend/bookhive/src/main/java/com/chema/bookhive/repository/BookRepository.java
+++ backend/bookhive/src/main/java/com/chema/bookhive/repository/BookRepository.java
@@ -1,7 +1,12 @@
 package com.chema.bookhive.repository;
 
 import com.chema.bookhive.model.Book;
+import com.chema.bookhive.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByUser(User user);
 }
--- backend/bookhive/src/main/java/com/chema/bookhive/repository/ShelfRepository.java
+++ backend/bookhive/src/main/java/com/chema/bookhive/repository/ShelfRepository.java
@@ -1,7 +1,12 @@
 package com.chema.bookhive.repository;
 
 import com.chema.bookhive.model.Shelf;
+import com.chema.bookhive.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ShelfRepository extends JpaRepository<Shelf, Long> {
+
+    List<Shelf> findByUser(User user);
 }
