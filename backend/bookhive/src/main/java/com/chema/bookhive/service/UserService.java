419b02784f3f6078a175f5d983b891b99382d41b
José María <josemariagodoy1996@gmail.com>
2026-09-23 14:34:04 +0200

    feat(UserServiceTest): Creo una clase para los test de userService

--- backend/bookhive/src/main/java/com/chema/bookhive/service/UserService.java
+++ backend/bookhive/src/main/java/com/chema/bookhive/service/UserService.java
@@ -1,4 +1,33 @@
 package com.chema.bookhive.service;
 
+import com.chema.bookhive.exception.UserNotFoundException;
+import com.chema.bookhive.exception.UsernameAlreadyExistsException;
+import com.chema.bookhive.model.User;
+import com.chema.bookhive.repository.UserRepository;
+import org.springframework.transaction.annotation.Transactional;import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("Username not found: " + username));
+    }
+
+    @Transactional
+    public User createUser(String username, String email, String password) {
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new UsernameAlreadyExistsException("Username in user: " + username);
+        }
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(password);
+        return userRepository.save(user);
+    }
 }
