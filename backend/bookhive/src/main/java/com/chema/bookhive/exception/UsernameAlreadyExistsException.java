37a817943f9eaa5384ae0dc62523b13a57c6d6e5
José María <josemariagodoy1996@gmail.com>
2026-09-23 14:30:37 +0200

    feat(exception.p, UsernameAlreadyExists.java, UserNotFOundException.java, UserService.java): Creadas la carpeta exception para el manejo de errores dos clases una Username exist y User no encontrado se decide hacer clases especificas ya que el proyecto no tengo pensado de que en principio vaya a ser muy grande y en UserService creo dos clases el Create user y el finduser

---
+++ backend/bookhive/src/main/java/com/chema/bookhive/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.chema.bookhive.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
